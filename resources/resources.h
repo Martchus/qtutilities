8a05677bb348c93419ae3c59cd2cf74b9073f3b1
Martchus <martchus@gmx.net>
2015-08-25 19:16:09 +0200

    updated application info

--- resources/resources.h
+++ resources/resources.h
@@ -10,13 +10,13 @@ class QString;
 class QStringList;
 QT_END_NAMESPACE
 
-#define SET_APPLICATION_INFO \
+#define SET_QT_APPLICATION_INFO \
     QCoreApplication::setOrganizationName(QStringLiteral(APP_AUTHOR)); \
     QCoreApplication::setOrganizationDomain(QStringLiteral(APP_URL)); \
     QCoreApplication::setApplicationName(QStringLiteral(APP_NAME)); \
     QCoreApplication::setApplicationVersion(QStringLiteral(APP_VERSION))
 
-#define LOAD_TRANSLATIONS \
+#define LOAD_QT_TRANSLATIONS \
     TranslationFiles::loadQtTranslationFile(); \
     TranslationFiles::loadApplicationTranslationFile(QStringLiteral(PROJECT_NAME))
 
