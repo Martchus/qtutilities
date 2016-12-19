60403202df6c8dbd46b8addc1978dc0daf635348
Martchus <martchus@gmx.net>
2016-12-19 23:38:32 +0100

    Add function to convert QString to native filename

---
+++ misc/conversion.h
@@ -0,0 +1,41 @@
+#ifndef CONVERSION_H
+#define CONVERSION_H
+
+#include "../global.h"
+
+#include <QString>
+
+#include <string>
+
+namespace ConversionUtilities {
+
+inline QByteArray toNativeFileName(const QString &fileName)
+{
+#if !defined(PLATFORM_MINGW) || !defined(CPP_UTILITIES_USE_NATIVE_FILE_BUFFER)
+    return fileName.toLocal8Bit();
+#else
+    return fileName.toUtf8();
+#endif
+}
+
+inline QString fromNativeFileName(const char *nativeFileName, int size = -1)
+{
+#if !defined(PLATFORM_MINGW) || !defined(CPP_UTILITIES_USE_NATIVE_FILE_BUFFER)
+    return QString::fromLocal8Bit(nativeFileName, size);
+#else
+    return QString::fromUtf8(nativeFileName, size);
+#endif
+}
+
+inline QString fromNativeFileName(const std::string &nativeFileName)
+{
+#if !defined(PLATFORM_MINGW) || !defined(CPP_UTILITIES_USE_NATIVE_FILE_BUFFER)
+    return QString::fromLocal8Bit(nativeFileName.data(), static_cast<int>(nativeFileName.size()));
+#else
+    return QString::fromUtf8(nativeFileName.data(), static_cast<int>(nativeFileName.size()));
+#endif
+}
+
+} // namespace Dialogs
+
+#endif // CONVERSION_H
