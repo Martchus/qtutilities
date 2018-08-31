868445184374a04b6b2d0aaf24dcf448da7ef748
Martchus <martchus@gmx.net>
2018-08-31 22:44:37 +0200

    Find icons in QML files

--- scripts/required_icons.sh
+++ scripts/required_icons.sh
@@ -15,9 +15,20 @@ for srcdir in "$@"; do
             icon_names["${iconset: 7 : -1}"]=1
         fi
     done
+
     # find icons in *.cpp files
     for from_theme_call in $(find "$srcdir" -iname '*.cpp' -print0 | xargs -0 cat | grep -Po 'QIcon::fromTheme\(QStringLiteral\(\".*?\"\)'); do
         icon_names["${from_theme_call: 33 : -2}"]=1
+    done
+
+    # find icons in *.qml files
+    for icon_name in $(find "$srcdir" -iname '*.qml' -print0 | xargs -0 cat | grep -Pzo '(?<=icon\.name\: |iconName\: |source\: )(\".+\"|(.|\n)* \? \".+\" : \".+\")'); do
+        # FIXME: improve pattern to get rid of of this
+        [ "${#icon_name}"         -ge 3 ] || continue
+        [ "${icon_name: 0 : 1}"  == '"' ] || continue
+        [ "${icon_name: -1 : 1}" == '"' ] || continue
+        [ "${icon_name: 1 : 1}"  == '#' ] && continue
+        icon_names["${icon_name: 1 : -1}"]=1
     done
 done
 
