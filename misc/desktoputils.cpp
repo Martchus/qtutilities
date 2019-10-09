df03b97cedc3c9efeedece600b38c9336254ca32
Martchus <martchus@gmx.net>
2019-10-09 12:54:35 +0200

    Fix openLocalFileOrDir() on Windows
    Not tested yet

--- misc/desktoputils.cpp
+++ misc/desktoputils.cpp
@@ -3,27 +3,40 @@
 #include <QDesktopServices>
 #include <QUrl>
 
+#ifdef Q_OS_WIN32
+#include <filesystem>
+#endif
+
 namespace QtUtilities {
 
 /*!
  * \brief Shows the specified file or directory using the default file browser.
- * \remarks \a path musn't be specified as URL. (Conversion to URL is the
- * purpose of this function).
+ * \remarks
+ * - The specified \a path must *not* be specified as URL. (The conversion to a URL suitable for
+ *   QDesktopServices::openUrl() is the whole purpose of this function).
+ * - The Qt documentation suggests to use
+ *   `QDesktopServices::openUrl(QUrl("file:///C:/Documents and Settings/All Users/Desktop", QUrl::TolerantMode));`
+ *   under Windows. However, that does not work if the path contains a '#'. It is also better to use
+ *   QUrl::DecodedMode to prevent QUrl from interpreting any of the paths characters in a special way.
  */
 bool openLocalFileOrDir(const QString &path)
 {
+    QUrl url(QStringLiteral("file://"));
+
 #ifdef Q_OS_WIN32
-    // backslashes are commonly used under Windows
-    // -> replace backslashes with slashes to support Windows paths
+    // replace backslashes with regular slashes
     QString tmp(path);
     tmp.replace(QChar('\\'), QChar('/'));
-    QUrl url(QStringLiteral("file:///"));
+
+    // add a slash before the drive letter of an absolute path
+    if (std::filesystem::path(path.toStdString()).is_absolute()) {
+        tmp = QStringLiteral("/") + tmp;
+    }
     url.setPath(tmp, QUrl::DecodedMode);
-    return QDesktopServices::openUrl(url);
+
 #else
-    QUrl url(QStringLiteral("file://"));
     url.setPath(path, QUrl::DecodedMode);
-    return QDesktopServices::openUrl(url);
 #endif
+    return QDesktopServices::openUrl(url);
 }
 } // namespace QtUtilities
