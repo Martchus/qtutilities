16eb2c2243daad065bb2c0a0f4a0e6c5aeca06bd
Martchus <martchus@gmx.net>
2025-01-06 16:11:22 +0100

    Update translations

--- translations/qtutilities_en_US.ts
+++ translations/qtutilities_en_US.ts
@@ -20,7 +20,7 @@
 <context>
     <name>QtGui::QtLanguageOptionPage</name>
     <message>
-        <location filename="../settingsdialog/qtsettings.cpp" line="588"/>
+        <location filename="../settingsdialog/qtsettings.cpp" line="619"/>
         <source>recognized by Qt as</source>
         <translation type="unfinished"></translation>
     </message>
@@ -28,7 +28,7 @@
 <context>
     <name>QtGui::QtOptionCategory</name>
     <message>
-        <location filename="../settingsdialog/qtsettings.cpp" line="442"/>
+        <location filename="../settingsdialog/qtsettings.cpp" line="473"/>
         <source>Qt</source>
         <translation type="unfinished"></translation>
     </message>
--- translations/qtutilities_zh_CN.ts
+++ translations/qtutilities_zh_CN.ts
@@ -20,7 +20,7 @@
 <context>
     <name>QtGui::QtLanguageOptionPage</name>
     <message>
-        <location filename="../settingsdialog/qtsettings.cpp" line="588"/>
+        <location filename="../settingsdialog/qtsettings.cpp" line="619"/>
         <source>recognized by Qt as</source>
         <translation>被 Qt 识别为</translation>
     </message>
@@ -28,7 +28,7 @@
 <context>
     <name>QtGui::QtOptionCategory</name>
     <message>
-        <location filename="../settingsdialog/qtsettings.cpp" line="442"/>
+        <location filename="../settingsdialog/qtsettings.cpp" line="473"/>
         <source>Qt</source>
         <translation>Qt</translation>
     </message>
