326caf9c891b82f079522b7c46cc3fd5c575db63
Martchus <martchus@gmx.net>
2025-05-02 21:33:37 +0200

    Allow native implementation of `openLocalFileOrDir()` on Android

--- misc/desktoputils.cpp
+++ misc/desktoputils.cpp
@@ -2,12 +2,21 @@
 
 #include <QDesktopServices>
 #include <QUrl>
+
 #if (QT_VERSION >= QT_VERSION_CHECK(6, 5, 0))
 #include <QGuiApplication>
 #include <QStyleHints>
 #endif
+
 #ifdef Q_OS_WIN32
 #include <QFileInfo>
+#endif
+
+#if defined(Q_OS_ANDROID) && (QT_VERSION >= QT_VERSION_CHECK(6, 4, 0))
+#define QT_UTILITIES_OPEN_VIA_ACTIVITY
+#include <QCoreApplication>
+#include <QJniEnvironment>
+#include <QJniObject>
 #endif
 
 namespace QtUtilities {
@@ -16,14 +25,26 @@ namespace QtUtilities {
  * \brief Shows the specified file or directory using the default file browser.
  * \remarks
  * - The specified \a path must *not* be specified as URL. (The conversion to a URL suitable for
- *   QDesktopServices::openUrl() is the whole purpose of this function).
+ *   QDesktopServices::openUrl() is the main purpose of this function).
  * - The Qt documentation suggests to use
  *   `QDesktopServices::openUrl(QUrl("file:///C:/Documents and Settings/All Users/Desktop", QUrl::TolerantMode));`
  *   under Windows. However, that does not work if the path contains a '#'. It is also better to use
  *   QUrl::DecodedMode to prevent QUrl from interpreting any of the paths characters in a special way.
+ * - Tries to open the path via the activity method `openPath()` under Android so that method can be implemented
+ *   to override the behavior if needed. The method must take a string and return a boolean. If it does not exist
+ *   the usual QDesktopServices::openUrl() is done.
  */
 bool openLocalFileOrDir(const QString &path)
 {
+#ifdef QT_UTILITIES_OPEN_VIA_ACTIVITY
+    if (const auto context = QNativeInterface::QAndroidApplication::context(); context.isValid()) {
+        auto env = QJniEnvironment();
+        if (auto openPathMethod = env.findMethod(context.objectClass(), "openPath", "(Ljava/lang/String;)Z")) {
+            return env->CallBooleanMethod(context.object(), openPathMethod, QJniObject::fromString(path).object()) == JNI_TRUE;
+        }
+    }
+#endif
+
     QUrl url(QStringLiteral("file://"));
 
 #ifdef Q_OS_WIN32
