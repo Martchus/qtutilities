3ae7618657d71fe2d58b2d1ab63268448aef4d10
Martchus <martchus@gmx.net>
2025-11-03 21:05:09 +0100

    Make it a hard config failure if expected plugins cannot be imported

--- cmake/modules/QtConfig.cmake
+++ cmake/modules/QtConfig.cmake
@@ -348,6 +348,8 @@ if (STATIC_LINKAGE OR QT_TARGET_TYPE STREQUAL STATIC_LIBRARY)
         # allow importing TLS plugins via qtconfig.h
         if (USED_TLS_PLUGINS)
             list_to_string(" " "\\\n    Q_IMPORT_PLUGIN(Q" ")" "${USED_TLS_PLUGINS}" USED_TLS_PLUGINS_ARRAY)
+        else ()
+            message(FATAL_ERROR "No TLS plugin could be imported but TLS_SUPPORT is enabled.")
         endif ()
     endif ()
 
@@ -380,6 +382,8 @@ if (STATIC_LINKAGE OR QT_TARGET_TYPE STREQUAL STATIC_LIBRARY)
         if (USED_NETWORK_INFORMATION_PLUGINS)
             list_to_string(" " "\\\n    Q_IMPORT_PLUGIN(Q" "BackendFactory)" "${USED_NETWORK_INFORMATION_PLUGINS}"
                            USED_NETWORK_INFORMATION_PLUGINS_ARRAY)
+        else ()
+            message(FATAL_ERROR "No TLS plugin could be imported but NETWORK_INFORMATION_SUPPORT is enabled.")
         endif ()
     endif ()
 
